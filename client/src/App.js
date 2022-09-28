90ac5509852618a1be20d51107096cb73d0bd8c6
Aviram Salamon <aviram83@gmail.com>
2022-09-28 18:50:47 +0300

    Add workspace

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <div className="workspace">
+        <div className="content">
+
+        </div>
+        <div className="actions">
+          <button>Add New Transaction</button>
+          <button>Compress Transactions</button>
+        </div>
+      </div>
     </div>
   );
 }
