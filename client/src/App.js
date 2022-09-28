c54d2ead7ad5cafad8b8636c7a9b77f266a85f19
Aviram Salamon <aviram83@gmail.com>
2022-09-28 19:08:43 +0300

    Add Table component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
+import Table from './components/Table/Table';
+
 import './App.css';
+
+const transactions = [
+  { tradingParty: "me", counterparty: "you", amount: -400 },
+  { tradingParty: "me", counterparty: "you", amount: 500 },
+  { tradingParty: "me", counterparty: "someone_else", amount: 100 },
+]
+
 
 function App() {
   return (
     <div className="App">
       <div className="workspace">
         <div className="content">
-
+          <Table 
+            label="Paying"
+            rows={transactions}
+          />
+          <Table
+            label="Receiving"
+            rows={transactions}
+          />
         </div>
         <div className="actions">
           <button>Add New Transaction</button>
--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,5 +1,34 @@
-const Table = () => {
-    <div>Table</div>
+import React from "react";
+
+import "./Table.css";
+
+const Table = ({ label, rows = [] }) => {
+
+    return (
+        <div className="table">
+            <div className="label">{label}</div>
+            <div className="inner-table">
+                <div className="table-header">
+                    <div className="column">Counterparty Name</div>
+                    <div className="column">Amount</div>
+                </div>
+                <div className="table-body">
+                    {
+                        rows.map((row, idx) => (
+                            <div className="row">
+                                <div className="cell">
+                                    {idx + 1}. {row.counterparty}
+                                </div>
+                                <div className="cell">
+                                    {row.amount}
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Table;
+export default Table
