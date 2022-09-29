f4a8d4f7c1222e5ab6669110634e8fbbf6755a19
Aviram Salamon <aviram83@gmail.com>
2022-09-29 15:30:28 +0300

    Add option to build the client so running the server is enough

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from "express";
-import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 import { setTransactions } from "./data/DB.js";
 import { getTransactions, addNewTransaction } from "./api/transactions.js";
 import { initilData } from "./data/mockData.js";
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.urlencoded());
 app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 setTransactions(initilData);
 
@@ -19,4 +24,8 @@ app.post("/api/transaction", addNewTransaction);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
