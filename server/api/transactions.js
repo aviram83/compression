15279a7e990a027bb3c50c3bb4b681033cf30cab
Aviram Salamon <aviram83@gmail.com>
2022-09-29 17:27:36 +0300

    fix date display on server requests

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -1,14 +1,14 @@
 import { setTransaction, data } from "../data/DB.js";
 
 export const getTransactions = (req, res) => {
-    const date = Date.now();
-    console.log(`${date} Fetch data from service "/api/transactions"`);
+    const date = new Date();
+    console.log(`${date.toLocaleString('en-GB')} - Fetch data from service "/api/transactions"`);
     res.json({ data: data.transactions });
 }
 
 export const addNewTransaction = (req, res) => {    
-    const date = Date.now();
-    console.log(`${date} Add new transaction with service "/api/transaction"`);
+    const date = new Date();
+    console.log(`${date.toLocaleString('en-GB')} - Add new transaction with service "/api/transaction"`);
     setTransaction(req.body);
 
     res.json({ success: true });
