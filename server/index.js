e4b38f5408f134d7abc3f0225d1c7bc49ece548e
Aviram Salamon <aviram83@gmail.com>
2022-09-29 09:43:57 +0300

    Add api of getting transactions

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const express = require("express");
+import express from "express";
+
+import { getTransactions } from "./api/transactions.js";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/api/transactions", getTransactions);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
